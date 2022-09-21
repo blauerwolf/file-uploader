4c8ddb775ac61684a33f8ee688413793ee49ab0b
Ernesto Ardenghi <eardenghi@gmail.com>
2022-09-21 14:54:56 -0300

    Commit

--- src/database/models/tratamiento.js
+++ src/database/models/tratamiento.js
@@ -2,10 +2,42 @@
 
 module.exports = (sequelize, DataTypes) => {
     let Tratamiento = sequelize.define('tratamiento', {
+        id: {
+            type: DataTypes.BIGINT,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            field: 'deleted_at'
+        }
+    }, { paranoid: true,
+        freezeTableName: true    
+
+    })
+
 
     }, { paranoid: true,
         freezeTableName: true
     })
+
 
     return Tratamiento
 }
