36a1a37c772289118cef701998ea1941fd7c62a4
blauerwolf <eardenghi@gmail.com>
2022-09-21 11:26:09 -0300

    Tabla medico agregada

---
+++ src/database/models/medico.js
@@ -0,0 +1,44 @@
+'use strict'
+
+module.exports = (sequelize, DataTypes) => {
+    let Medico = sequelize.define('medico', {
+        id: {
+            type: DataTypes.BIGINT,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false
+        },
+        nombre: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        dni: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        createdAt: {
+            type: DataTypes.DATE,
+            field: 'created_at',
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        updatedAt: {
+            type: DataTypes.DATE,
+            field: 'updated_at',
+            defaultValue: DataTypes.NOW,
+            allowNull: false
+        },
+        deletedAt: {
+            type: DataTypes.DATE,
+            field: 'deleted_at'
+        }
+    }, { paranoid: true,
+        freezeTableName: true    
+    })
+
+    Medico.associate = models => {
+        Medico.hasMany(models.paciente)
+    }
+
+    return Medico
+}
